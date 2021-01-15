9dde6c94600133580330ccd0d4340bbc2337fd7b
nurbolatk <nurbolattk@gmail.com>
2021-01-15 14:12:45 +0600

    Finish toggling nav links in mobile

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,18 @@
 let isMobileNavShown = false
 const btnHamburger = document.getElementById('btn-hamburger')
+const navHeader = document.querySelector('.header__links')
+const overlay = document.querySelector('.overlay')
 
-btnHamburger.addEventListener('click', function () {
+btnHamburger.addEventListener('click', toggleMobileNav)
+
+function toggleMobileNav() {
   btnHamburger.classList.toggle('open')
   isMobileNavShown = !isMobileNavShown
+
+  overlay.classList.toggle('hide')
+  navHeader.classList.toggle('show')
+}
+
+overlay.addEventListener('click', function (e) {
+  toggleMobileNav()
 })
