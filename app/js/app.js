1bfa512d5f40d078e1b8a2491b0234590e798fcb
nurbolatk <nurbolattk@gmail.com>
2021-01-15 13:07:35 +0600

    Animate hamburger menu on click

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,7 @@
-console.log('hi there')
+let isMobileNavShown = false
+const btnHamburger = document.getElementById('btn-hamburger')
+
+btnHamburger.addEventListener('click', function () {
+  btnHamburger.classList.toggle('open')
+  isMobileNavShown = !isMobileNavShown
+})
